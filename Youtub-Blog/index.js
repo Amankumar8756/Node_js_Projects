8141f355711aa4ddc579c17c829e1e563511b57d
Amankumar8756 <iit2022168@iiita.ac.in>
2025-02-13 17:12:38 +0530

    complete

---
+++ Youtub-Blog/index.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+
+const blogRoute = require("./routes/blog");
+const cookieParser = require('cookie-parser');
+
+const Blog = require('./models/blog');
+
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+
+const app = express(); 
+const PORT = process.env.PORT || 5000;
+
+// MongoDB Connection  
+mongoose
+  .connect("mongodb://localhost:27017/blogify")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
+
+// Template Engine Setup
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+// Middleware
+// app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(  checkForAuthenticationCookie ("token")); 
+app.use(express.static(path.resolve("./public")));
+// Routes
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+     blogs: allBlogs, 
+  });
+});
+app.use("/user", userRoute); 
+app.use("/blog", blogRoute); 
+
+
+// Start Server
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`)); 
